54e2caefa70266756411991cd9fffd864c9cbb51
Maharsh Patel <maharsh@live.ca>
2017-05-06 21:35:29 -0600

    html part 2 complete

--- verify/urls.py
+++ verify/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.index, name="index"),
+    url(r'^verify/$', views.verify, name="verify")
 ]
--- verify/views.py
+++ verify/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "verify/sms.html")
+
+def verify(request):
+    return render(request, "verify/verification.html")
