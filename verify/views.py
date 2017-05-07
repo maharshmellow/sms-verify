167c8683454515c1270f06c589a42940eb341f8a
Maharsh Patel <maharsh@live.ca>
2017-05-07 13:30:09 -0600

    printing out verification code

--- verify/views.py
+++ verify/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+import random
+import hashlib
+import os
 # Create your views here.
 def index(request):
     return render(request, "verify/sms.html")
@@ -7,5 +9,17 @@ def index(request):
 def verify(request):
     print("verify")
     phoneNumber = request.POST['phonenumber']
-    
-    return render(request, "verify/verification.html")
+    randomNumber = str(random.randint(1, 1000))
+    verificationCode = getVerificationCode(phoneNumber, randomNumber)
+
+    sendVerificationCode(verificationCode)
+
+    return render(request, "verify/verification.html", {"phone": phoneNumber, "random": randomNumber})
+
+
+def getVerificationCode(phoneNumber, randomNumber):
+    return(hashlib.sha512((phoneNumber+randomNumber+str(os.environ.get("SEED"))).encode("utf-8")).hexdigest()[:5])
+
+def sendVerificationCode(verificationCode):
+    print("Verification Code: ", verificationCode)
+    # TODO put the api call for sending the verification code here
