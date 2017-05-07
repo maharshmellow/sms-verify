40ed44c531f953072533a83d39d584ca3f22e6ba
Maharsh Patel <maharsh@live.ca>
2017-05-06 21:56:11 -0600

    input verification/error message

--- verify/static/verify/js/scripts.js
+++ verify/static/verify/js/scripts.js
@@ -4,3 +4,20 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
 } else {
 	$('#backplate').height($(window).height() - 100);
 }
+
+
+
+// // on successful verification
+// $("body").animate({
+// 	backgroundColor: "#20FFAD"
+// }, 1500);
+//
+// $("#message").css("color", "#20FFAD");
+// document.getElementById("message").innerHTML = "Verification Successful";
+
+// on failed verification
+$("body").animate({
+	backgroundColor: "#f5003c"
+}, 1500);
+$("#message").css("color", "#f5003c");
+document.getElementById("message").innerHTML = "Verification Failed";
