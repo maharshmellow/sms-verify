5b193a059f810362b09d3ebaeb41487a3231598f
Maharsh Patel <maharsh@live.ca>
2017-05-07 17:11:21 -0600

    everthing working - not sending sms yet

--- verify/urls.py
+++ verify/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^verify/$', views.verify, name="verify")
+    url(r'^verify/$', views.verify, name="verify"),
+    url(r'^checkCode/$', views.checkCode, name="checkCode")
 ]
--- verify/views.py
+++ verify/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import random
 import hashlib
 import os
@@ -11,19 +12,33 @@ def verify(request):
     # if the form wasn't submitted properly, then go to index page
     try:
         phoneNumber = request.POST['phonenumber']
+
     except:
         return render(request, "verify/sms.html")
 
-    verificationCode = getVerificationCode(phoneNumber)
+    # sent to the user - what the user has to type in
+    verificationCode = hashlib.sha512((phoneNumber+str(random.randint(1,1000))+str(os.environ.get("SEED"))).encode("utf-8")).hexdigest()[:5]
+    # sent to next page for the get request instead of phone number - to prevent brute forcing the validation
+    # verificatoinCode that the user types in will be hashed and checked against the checkSequence
+    checkSequence = hashlib.sha512((verificationCode+str(os.environ.get("SEED"))).encode("utf-8")).hexdigest()[:100]
 
     # send the sms to the user with the verification code
     sendVerificationCode(verificationCode)
+    return render(request, "verify/verification.html", {"code": checkSequence})
 
-    return render(request, "verify/verification.html", {"phone": phoneNumber})
+def checkCode(request):
+    try:
+        verificationCode = request.GET['verification']
+        correctCheckSequence = request.GET['code']
+    except:
+        return render(request, "verify/sms.html")
 
-
-def getVerificationCode(phoneNumber):
-    return(hashlib.sha512((phoneNumber+str(os.environ.get("SEED"))).encode("utf-8")).hexdigest()[:5])
+    # check the correct check sequence against the check sequence based on the verification code provided
+    checkSequence = hashlib.sha512((verificationCode+str(os.environ.get("SEED"))).encode("utf-8")).hexdigest()[:100]
+    if checkSequence == correctCheckSequence:
+        return HttpResponse("1")
+    else:
+        return HttpResponse("0")
 
 def sendVerificationCode(verificationCode):
     print("Verification Code: ", verificationCode)
