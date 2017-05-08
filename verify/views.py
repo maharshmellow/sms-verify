f23dcea6f564c2ac9d02345c0a19db1090784ef7
Maharsh Patel <maharsh@live.ca>
2017-05-07 21:09:41 -0600

    cleaned up

--- verify/views.py
+++ verify/views.py
@@ -6,27 +6,28 @@ import os
 import requests
 
 def index(request):
+    """Displays the page that asks for the phone number"""
     return render(request, "verify/sms.html")
 
 def verify(request):
-    print("verify")
+    """Displays the page that asks for the verification code"""
     # if the form wasn't submitted properly, then go to index page
     try:
         phoneNumber = request.POST['phonenumber']
-
     except:
         return render(request, "verify/sms.html")
 
     # sent to the user - what the user has to type in
     verificationCode = hashlib.sha512((phoneNumber+str(random.randint(1,1000))+str(os.environ.get("SEED"))).encode("utf-8")).hexdigest()[:5]
-    # sent to next page for the get request instead of phone number - to prevent brute forcing the validation
-    # verificatoinCode that the user types in will be hashed and checked against the checkSequence
+    # sent to next page for the get request instead of phone number
+    # verificationCode that the user types in will be hashed and checked against the checkSequence
     checkSequence = hashlib.sha512((verificationCode+str(os.environ.get("SEED"))).encode("utf-8")).hexdigest()[:100]
     # send the sms to the user with the verification code
     sendVerificationCode(phoneNumber, verificationCode)
     return render(request, "verify/verification.html", {"code": checkSequence})
 
 def checkCode(request):
+    """Checks if the verification code entered is correct"""
     try:
         verificationCode = request.GET['verification']
         correctCheckSequence = request.GET['code']
@@ -41,6 +42,7 @@ def checkCode(request):
         return HttpResponse("0")
 
 def sendVerificationCode(phoneNumber, verificationCode):
+    """Sends the verification code to the provided phone number using TILL API"""
     if len(phoneNumber) < 10:
         return              # doesn't give the user an error message - just doesn't send the message
 
